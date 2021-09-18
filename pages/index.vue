1e8c3d876c28caaa0bf5d6d4d91923db02f65a0b
Dhrubo Hasan <insular55@gmail.com>
2021-09-18 19:48:44 +0600

    adding link with a div

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,10 @@
   <main>
     <section class="self-center flex flex-col flex-1 items-center justify-center">
       <h1 class="title text-center">Mohibul Hassan Chowdhury</h1>
-      <h5 class="subtitle text-center">Working as a Software Engineer</h2>
-      
+      <h5 class="subtitle text-center">Working as a Software Engineer</h5>
+      <div class="self-center flex">
       <IconLinkedin class="icon" style="color: #777"/>
+      </div>
     </section>
     <section class="mt-8">
       <h3 class="text-primary-600 dark:text-primary-400 max-w-5xl mx-auto">Latest blog post</h3>
